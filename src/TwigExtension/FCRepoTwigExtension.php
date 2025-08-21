40251a0fa6ac29434ba08598d1a5ca9cbfaa2ec8
Jeremy M Gottwig <jgottwig@umd.edu>
2025-08-21 15:18:50 -0400

    LIBWEB-6490. Adding new twig function for getting query param.
    -Updating SDC as needed for View.
    
    https://umd-dit.atlassian.net/browse/LIBWEB-6490

---
+++ src/TwigExtension/FCRepoTwigExtension.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\umdlib_fcrepo_solr_helper\TwigExtension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Twig\TwigFilter;
+
+/**
+ * Twig extension providing custom functionalities.
+ *
+ * @package Drupal\umdlib_fcrepo_solr_helper\TwigExtension
+ */
+class FCRepoTwigExtension extends AbstractExtension {
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return 'umdlib_fcrepo_query.twig_extension';
+  }
+
+  public function getFunctions() {
+    return [
+        new TwigFunction('fc_url_query', [
+            $this,
+            'getUrlQuery'
+        ]),
+    ];
+  }
+
+  public function getUrlQuery() {
+    $full_uri = \Drupal::request()->getRequestUri();
+    if (!empty($full_uri) && str_contains($full_uri, 'query')) {
+      $trunc_uri = explode('?', $full_uri);
+      parse_str(!empty($trunc_uri[1]) ? $trunc_uri[1] : $full_uri, $uri_array);
+      foreach ($uri_array as $key => $value) {
+        if ($key == 'query') {
+          return $value;
+        }
+      }
+    }
+    return;
+  }
+
+  public function getFilters() {
+    return [
+      new TwigFilter(
+        'landing_page',
+        [$this, 'getCollectionLandingPage']
+      ),
+    ];
+  }
+
+  public function getCollectionLandingPage(string $collection) {
+    $lp_map = [
+      "UMD Student Newspapers" => "",
+      "Katherine Anne Porter Correspondence" => "Article",
+      "National Trust Library Postcards" => "Audio",
+      "Prange Children's Books" => "Audio",
+      "Prange Posters and Wall Newspapers" => "Audio Book",
+      "US Government Posters" => "Book",
+      "Diamondback Photographs" => "CD",
+      "Labor" => "Computer File",
+      "Advancing Workers' Rights" => "DVD",
+      "Commercial Broadcasting" => "eBook",
+      "Cultural Preservation" => "eMusic",
+      "Literature and Rare Books" => "eVideo",
+      "Music, Dance, and Theater at UMD" => "Image",
+      "Performing Arts" => "Journal",
+      "Politics and Civic Life" => "Journal",
+      "Postwar Japan" => "LP",
+      "Public Broadcasting" => "Map",
+      "Punk Collections" => "Newspaper",
+      "Maryland and Historical Collections" => "Score",
+      "thesis" => "Thesis",
+      "video_recording" => "Video Recording",
+      "other" => "Other",
+      "database" => "Database",
+      "web_page" => "Webpage",
+      "book_chapter" => "Book Chapter",
+      "conference_proceeding" => "Conference Proceeding",
+      "dissertation" => "Dissertation",
+      "kit" => "Kit",
+      "manuscript" => "Manuscript",
+      "text_resource" => "Text Resource",
+      "video" => "Video",
+      "web_resource" => "Web Resource",
+    ];
+    return !empty($lp_map[$collection]) ? $lp_map[$collection] : null;
+  }
+
+
+}
